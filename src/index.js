61379dbbbb21805e0181421f40108ba925424242
lv-conner <lvmh123456@gmail.com>
2018-12-14 15:18:09 +0800

    hot

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+import React, { Component } from 'react'
+import { render } from "react-dom";
+class Index extends Component{
+    render(){
+        return (
+            <div>Index Page Hello</div>
+        )
+    }
+}
+
+render(<Index/>,document.getElementById("root"));
